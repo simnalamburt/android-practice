5bbc7ffd16d6849fee8ca93b26524c705752f83c
Hyeon Kim <simnalamburt@gmail.com>
2014-12-10 22:22:33 +0900

    :tada: Initial commit

---
+++ app/src/androidTest/java/me/hyeon/androidpractice/ApplicationTest.java
@@ -0,0 +1,13 @@
+package me.hyeon.androidpractice;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
